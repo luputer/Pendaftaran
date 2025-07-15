5edd3fa3bcf38f1dec963791375a1d0a51b7a6d2
Muhammad Saidi <muhammad@example.com>
2025-07-15 15:31:12 +0800

    refactor: migrate to simple routeResorce

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\DosenController;
 use App\Http\Controllers\Gurucontroller;
 use App\Http\Controllers\SiswaController;
 
@@ -10,30 +10,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/guru',[Gurucontroller::class, 'index'])->name('guru.index');
-Route::get('/guru/tambah',[Gurucontroller::class, 'create'])->name('guru.create');
-Route::post('/guru/tambah',[Gurucontroller::class, 'store'])->name('guru.store');
-Route::get('/guru/{id}/edit', [GuruController::class, 'edit'])->name('guru.edit');
-Route::put('/guru/{id}', [GuruController::class, 'update'])->name('guru.update');
-Route::delete('/guru/{id}', [GuruController::class, 'destroy'])->name('guru.destroy');
+// Route::get('/guru', [Gurucontroller::class, 'index'])->name('guru.index');
+// Route::get('/guru/tambah', [Gurucontroller::class, 'create'])->name('guru.create');
+// Route::post('/guru/tambah', [Gurucontroller::class, 'store'])->name('guru.store');
+// Route::get('/guru/{id}/edit', [GuruController::class, 'edit'])->name('guru.edit');
+// Route::put('/guru/{id}', [GuruController::class, 'update'])->name('guru.update');
+// Route::delete('/guru/{id}', [GuruController::class, 'destroy'])->name('guru.destroy');
 
-
-Route::resource('dosen', App\Http\Controllers\DosenController::class);
-
-Route::get('/siswa', [SiswaController::class, 'index'])
-    ->name('siswa.index');
-Route::get(
-    '/siswa/tambah',
-    [SiswaController::class, 'create']
-)->name('siswa.create');
-
-Route::post('/siswa', [SiswaController::class, 'store'])
-    ->name('siswa.store');
-
-Route::get('/siswa/{siswa}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
-
-Route::put('/siswa/{id}', [SiswaController::class, 'update'])
-    ->name('siswa.update');
-
-Route::delete('/siswa/{siswa}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
-
+Route::resource('dosen', DosenController::class);
+Route::resource('guru', Gurucontroller::class);
+Route::resource('siswa', SiswaController::class);
